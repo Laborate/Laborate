a8ac6016f8f4e9f4c8d5e6840b8f5548d276f24a
root <root@codelaborate.(none)>
2013-01-28 06:28:37 +0000

    check if redirect function is called

--- server/php/user/restrict.php
+++ server/php/user/restrict.php
@@ -2,15 +2,17 @@
 require($_SERVER['DOCUMENT_ROOT'].'/server/php/core/config.php');
 require($_SERVER['DOCUMENT_ROOT'].'/server/php/core/database.php');
 
-function redirect() {
-    if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest' ) {
-        echo $GLOBALS['ajax_message'];
-        exit;
-    } else {
-        header("Location: /login?continue=http://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]);
-        exit;
-    }
+if(!function_exists('redirect')) {
+    function redirect() {
+        if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest' ) {
+            echo $GLOBALS['ajax_message'];
+            exit;
+        } else {
+            header("Location: /login?continue=http://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]);
+            exit;
+        }
 
+    }
 }
 
 $query_Sessions = "SELECT * FROM users WHERE users.user_id = '".$_SESSION['userId']."'";
