e92d0a7ab39330063fbfd4408ac408e31ed6fb81
Brian Vallelunga <bvallelunga@parkme.com>
2013-02-01 01:38:48 -0800

    moved all github funcitons to github php wrapper

--- server/php/locations/github_core.php
+++ server/php/locations/github_core.php
@@ -5,11 +5,12 @@ require($_SERVER['DOCUMENT_ROOT'].'/server/php/core/core.php');
 require($_SERVER['DOCUMENT_ROOT'].'/server/php/vendor/autoload.php');
 
 function startGithubClient() {
-    if(!isset($client) || is_null($client)) {
-        $client = new Github\Client();
+    //if(!isset($client) || is_null($client)) {
+        $client = new Github\Client(new Github\HttpClient\CachedHttpClient(array('cache_dir' => '/tmp/github-api-cache')));
         $client->authenticate($_SESSION['userGithub'], "", "http_token");
-        $client->getHttpClient()->setOption('Authorization: token '.$_SESSION['userGithub']);
-    }
+        $client->getHttpClient()->setOption('auth_method', "AUTH_HTTP_TOKEN");
+        $client->getHttpClient()->setOption('secret', $_SESSION['userGithub']);
+    //}
     return $client;
 }
 
@@ -24,22 +25,22 @@ function getRepositories() {
 }
 
 function getDirectory($repo, $dir="") {
-    $url = "https://api.github.com/repos/".$repo."/contents/".$dir;
-    $fields = array("access_token" => $_SESSION['userGithub']);
+    $client = startGithubClient();
+    $response = $client->getHttpClient()->get("repos/".$repo."/contents/".$dir)->getContent();
     $directory = array();
     if($dir) {
         array_push($directory, array("type"=> "back", "name" => "", "path" => substr($dir, 0, strrpos($dir, '/'))));
     }
-    foreach(jsonToArray(curlGet($url, $fields)) as $key => $value) {
+    foreach($response as $key => $value) {
         array_push($directory, array("type"=> $value['type'], "name" => $value['name'], "path" => $value['path']));
     }
+    echo $url;
     return json_encode($directory);
 }
 
 function getFile($repo, $path) {
-    $url = "https://api.github.com/repos/".$repo."/contents/".$path;
-    $fields = array("access_token" => $_SESSION['userGithub']);
-    $response = jsonToArray(curlGet($url, $fields));
+    $client = startGithubClient();
+    $response = $client->getHttpClient()->get("repos/".$repo."/contents/".$path)->getContent();
     $file = $response['content'];
     return base64_decode($file);
 }
