98f9514a9af6b89f043cc040a1772d3afdbc0fb6
Brian Vallelunga <bvallelunga@parkme.com>
2013-02-05 23:52:55 -0800

    daily update

--- shell/server/sql_update.sh
+++ shell/server/sql_update.sh
@@ -1,9 +1,9 @@
 if [ $1 == content ]
 then
-    mysql -p Codelaborate < ./sql_backups/update.sql < ./sql_backups/update_content.sql
+    mysql -p Codelaborate < ./sql_backups/update_content.sql
     echo "SQL Content and Structure Successfully Updated";
 elif [ $1 == structure ]
 then
-    mysql -p Codelaborate < ./sql_backups/update.sql < ./sql_backups/update_structure.sql
+    mysql -p Codelaborate < ./sql_backups/update_structure.sql
     echo "SQL Structure Successfully Updated";
 fi
