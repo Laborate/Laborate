201ffa416df6369cb8d5f33c21630248054c45fd
Brian Vallelunga <bvallelunga@parkme.com>
2013-02-24 01:39:19 -0800

    daily update

--- server/php/template/dependencies.php
+++ server/php/template/dependencies.php
@@ -76,35 +76,35 @@ function placeDependencies() {
     if($_SESSION['no_cache'] == true) { $cache = '&nc='.rand(0, 1000000000); }
     else { $cache = ""; }
 
-    echo ('<!-- Opera Speed Dial Favicon -->
-    <link rel="icon" type="image/png" href="/favicon/160.png" />
+    printf('<!-- Opera Speed Dial Favicon -->
+    <link rel="icon" type="image/png" href="/favicon/160.png" >
     <!-- Standard Favicon -->
-    <link rel="icon" type="image/png" href="/favicon/16.png" />
+    <link rel="icon" type="image/png" href="/favicon/16.png" >
     <!-- For iPhone 4 Retina display: -->
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/favicon/114.png">
     <!-- For iPad: -->
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/favicon/72.png">
     <!-- For iPhone: -->
-    <link rel="apple-touch-icon-precomposed" href="/favicon/57.png">');
+    <link rel="apple-touch-icon-precomposed" href="/favicon/57.png">%1$s', PHP_EOL);
 
     if($GLOBALS['css']) {
-        echo '<link href="/min/?b=css&amp;f='.implode(",", array_unique($GLOBALS['css'])).$cache.'" rel="stylesheet" type="text/css"/>';
+        printf('<link href="/min/?b=css&amp;f='.implode(",", array_unique($GLOBALS['css'])).$cache.'" rel="stylesheet" type="text/css">%1$s', PHP_EOL);
     }
 
     if($GLOBALS['codeMirror_css']) {
-        echo '<link href="/min/?b=lib&amp;f='.implode(",", array_unique($GLOBALS['codeMirror_css'])).$cache.'" rel="stylesheet" type="text/css"/>';
+        printf('<link href="/min/?b=lib&amp;f='.implode(",", array_unique($GLOBALS['codeMirror_css'])).$cache.'" rel="stylesheet" type="text/css">%1$s', PHP_EOL);
     }
 
     if($GLOBALS['js'] || $GLOBALS['codeMirror_js']) {
-        echo '<script src="/js/core/jquery.js" type="text/javascript"></script>';
+        printf('<script src="/js/core/jquery.js" type="text/javascript"></script>%1$s', PHP_EOL);
     }
 
     if($GLOBALS['js']) {
-        echo '<script src="/min/?b=js&amp;f='.implode(",", array_unique($GLOBALS['js'])).$cache.'" type="text/javascript"></script>';
+        printf('<script src="/min/?b=js&amp;f='.implode(",", array_unique($GLOBALS['js'])).$cache.'" type="text/javascript"></script>%1$s', PHP_EOL);
     }
 
     if($GLOBALS['codeMirror_js']) {
-        echo '<script src="/min/?b=lib&amp;f='.implode(",", array_unique($GLOBALS['codeMirror_js'])).$cache.'" type="text/javascript"></script>';
+        printf('<script src="/min/?b=lib&amp;f='.implode(",", array_unique($GLOBALS['codeMirror_js'])).$cache.'" type="text/javascript"></script>%1$s', PHP_EOL);
     }
 }
 
