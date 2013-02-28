28c51a15168da954e6312375d05f46a89491d196
Brian Vallelunga <bvallelunga@parkme.com>
2013-02-28 00:10:33 -0800

    daily update

--- init.sh
+++ init.sh
@@ -1,43 +1,38 @@
-#!/bin/sh -e
-
 #Start Up
-BASE = "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-#Login As root
-sudo su
+BASE=$(cd "$(dirname "$0")"; pwd)
 
 #Update APT-GET
-echo 'System Update'
+echo -e '\033[32m System Update \033[m'
 apt-get -y update
 apt-get install curl
 apt-get -y install libssl-dev pkg-config build-essential curl gcc g++ checkinstall
-echo 'Update Completed'
+echo -e '\033[32m Update Completed'
 
 #Install Apache2
-echo 'Installing Apache2'
+echo -e '\033[32m Installing Apache2'
 apt-get install apache2
-echo 'Apache2 Install Complete'
+echo -e '\033[32m Apache2 Install Complete'
 
 #Install Mysql
-echo 'Installing Mysql'
+echo -e '\033[32m Installing Mysql \033[m'
 apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
 mysql_install_db
-echo 'Mysql Install Complete'
+echo -e '\033[32m Mysql Install Complete \033[m'
 
 #Install PHP
-echo 'Installing PHP'
+echo -e '\033[32m Installing PHP \033[m'
 apt-get install php5 libapache2-mod-php5
-echo 'PHP Install Complete'
+echo -e '\033[32m PHP Install Complete \033[m'
 
 #Install PHP Modules
-echo 'Installing PHP Modules'
+echo -e '\033[32m Installing PHP Modules \033[m'
 apt-get install php5-cgi php5-cli php5-common php5-curl php5-dbg php5-mcrypt
 apt-get install php5-dev php5-mysql php5-odbc php5-fpm libphp5-embed
 apt-get install php5-mysqlnd php5-memcache php5-imap php5-geoip php5-ldap
-echo 'PHP Modules Install Complete'
+echo -e '\033[32m PHP Modules Install Complete \033[m'
 
 #Install Nodes
-echo 'Installing Node'
+echo -e '\033[32m Installing Node \033[m'
 mkdir /tmp/node-install
 cd /tmp/node-install
 wget http://nodejs.org/dist/v0.8.19/node-v0.8.19.tar.gz
@@ -45,36 +40,39 @@ tar -zxf node-v0.8.19.tar.gz
 cd node-v0.8.19
 ./configure && make && checkinstall --install=yes --pkgname=nodejs --pkgversion "0.8.19" --default
 cd $BASE
-echo 'Node Install Complete'
+echo -e '\033[32m Node Install Complete \033[m'
 
 #Install Node Modules
-echo 'Installing Node Modules'
+echo -e '\033[32m Installing Node Modules \033[m'
 npm install mysql
 npm install mysql-queues
 npm install socket.io
-echo 'Node Modules Install Complete'
+echo -e '\033[32m Node Modules Install Complete' \033[m
 
 #Install Vendor Modules
-echo 'Installing Vendor Modules'
+echo -e '\033[32m Installing Vendor Modules \033[m'
 cd $BASE/server/php/
 curl -s http://getcomposer.org/installer | php
 php composer.phar install
 cd $BASE
-echo 'Vendor Modules Install Complete'
+echo -e '\033[32m Vendor Modules Install Complete \033[m'
 
 #Install Vim
-echo 'Installing Vim'
+echo -e '\033[32m Installing Vim \033[m'
 apt-get remove vim-tiny
 apt-get install vim
-echo 'Vim Install Complete'
+echo -e '\033[32m Vim Install Complete \033[m'
 
 #Configuring User Preferences
+echo -e '\033[32m Configuring User Preferences \033[m'
 git config --global color.ui auto
 git config --global core.editor "vim"
 export VISUAL=vim
 export EDITOR=vim
+echo -e '\033[32m Configured User Preferences \033[m'
 
 #Clean Up Install
+echo -e '\033[32m Cleaning Up Install \033[m'
 cd $BASE/server/php/
 rm composer.lock
 rm composer.phar
@@ -83,4 +81,5 @@ rm composer.json
 cd $BASE
 git update-index --assume-unchanged init.sh
 rm $0
-echo 'Installer Finished'
+echo -e '\033[32m Installer Finished \033[m'
+exit
