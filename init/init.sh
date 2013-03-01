d78811191b8b800c796867deab3b1c608e43ff84
Brian Vallelunga <bvallelunga@parkme.com>
2013-03-01 12:07:43 -0800

    daily update

--- init/init.sh
+++ init/init.sh
@@ -19,6 +19,31 @@ sudo apt-get -y install vsftpd
 cp $BASE/init/vsftpd.conf /etc/vsftpd.conf -fr
 echo -e '\033[32mFTP Install Complete\033[m'
 
+#Install Apache2
+echo -e '\033[32mInstalling Apache2\033[m'
+apt-get -y install apache2
+cp $BASE/init/208.68.39.56 /etc/apache2/sites-available/208.68.39.56 -fr
+a2ensite 208.68.39.56
+echo -e '\033[32mApache2 Install Complete\033[m'
+
+#Install Apache2 Modules
+echo -e '\033[32mInstalling Apache2 Modules\033[m'
+apt-get -y install libapache2-mod-auth-plain
+apt-get -y install libapache2-mod-proxy-html
+apt-get -y install libapache2-mod-php5filter
+apt-get -y install libapache2-mod-uwsgi
+apt-get -y install libapache2-mod-vhost-hash-alias
+apt-get -y install libapache2-webauth
+apt-get -y install libapr-memcache0
+apt-get -y install libapr-memcache-dev
+apt-get -y install libapache2-mod-auth-mysql
+a2enmod rewrite
+a2enmod suexec
+a2enmod include
+a2enmod userdir
+a2enmod deflate
+echo -e '\033[32mApache2 Modules Install Complete\033[m'
+
 #Install Mysql
 echo -e '\033[32mInstalling Mysql \033[m'
 apt-get -y install mysql-server
@@ -26,18 +51,19 @@ mysql_install_db
 echo -e '\033[32mMysql Install Complete \033[m'
 
 #Populate Database
-echo -e '\033[32mUpdating Database \033[m'
+echo -e '\033[32mPopulating Database \033[m'
 mysql --user="root" --password="bjv0623" -e "CREATE DATABASE Codelaborate"
 cp $BASE/sql_backups/update_structure.sql.bz2 $BASE/sql_backups/update_structure2.sql.bz2
 bunzip2 $BASE/sql_backups/update_structure.sql.bz2
 mysql --user="root" --password="bjv0623" Codelaborate < $BASE/sql_backups/update_structure.sql
 mv $BASE/sql_backups/update_structure2.sql.bz2 $BASE/sql_backups/update_structure.sql.bz2
 rm $BASE/sql_backups/update_structure.sql
-echo -e '\033[32mDatabase Updated \033[m'
+echo -e '\033[32mDatabase Populated \033[m'
 
 #Install PHP
 echo -e '\033[32mInstalling PHP \033[m'
 apt-get -y install php5
+cp $BASE/init/php.ini /etc/php5/apache2/php.ini -fr
 echo -e '\033[32mPHP Install Complete \033[m'
 
 #Install PHP Modules
@@ -54,35 +80,6 @@ cp $BASE/init/composer.json composer.json
 curl -s http://getcomposer.org/installer | php
 php composer.phar install
 echo -e '\033[32mPHP Vendor Modules Install Complete \033[m'
-
-#Install Apache2
-echo -e '\033[32mInstalling Apache2\033[m'
-apt-get -y install apache2
-cp $BASE/init/208.68.39.56 /etc/apache2/sites-available/208.68.39.56 -fr
-cp $BASE/init/php.ini /etc/php5/apache2/php.ini -fr
-a2ensite 208.68.39.56
-echo -e '\033[32mApache2 Install Complete\033[m'
-
-#Install Apache2 Modules
-echo -e '\033[32mInstalling Apache2 Modules\033[m'
-apt-get -y remove libapache2-mod-php5
-apt-get -y install libapache2-mod-php5
-apt-get -y install libapache2-mod-auth-plain
-apt-get -y install libapache2-mod-proxy-html
-apt-get -y install libapache2-mod-php5filter
-apt-get -y install libapache2-mod-uwsgi
-apt-get -y install libapache2-mod-vhost-hash-alias
-apt-get -y install libapache2-webauth
-apt-get -y install libapr-memcache0
-apt-get -y install libapr-memcache-dev
-apt-get -y install libapache2-mod-auth-mysql
-a2enmod php5
-a2enmod rewrite
-a2enmod suexec
-a2enmod include
-a2enmod userdir
-a2enmod deflate
-echo -e '\033[32mApache2 Modules Install Complete\033[m'
 
 #Install Nodes
 echo -e '\033[32mInstalling Node \033[m'
@@ -120,7 +117,6 @@ echo -e '\033[32mCleaning Up Install \033[m'
 rm $BASE/server/php/composer.lock
 rm $BASE/server/php/composer.phar
 rm $BASE/server/php/composer.json
-git checkout $BASE/.htaccess
 echo -e '\033[32mInstaller Finished \033[m'
 cd $BASE
 exit
