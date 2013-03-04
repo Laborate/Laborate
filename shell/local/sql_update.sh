f47fe9c7b975eca588495b457b184aba8f37f84e
Brian Vallelunga <b.vallelunga@yahoo.com>
2013-03-04 01:50:45 -0800

    daily update

--- shell/local/sql_update.sh
+++ shell/local/sql_update.sh
@@ -2,7 +2,7 @@ if [ $1 == content ]
 then
     cp ./sql_backups/update_content.sql.bz2 ./sql_backups/update_content2.sql.bz2
     bunzip2 ./sql_backups/update_content.sql.bz2
-    /Applications/MAMP/Library/bin/mysql --user=root --password=bjv0623 Codelaborate < ./sql_backups/update_content.sql
+    /Applications/MAMP/Library/bin/mysql --user=root --password=bjv0623 code < ./sql_backups/update_content.sql
     mv ./sql_backups/update_content2.sql.bz2 ./sql_backups/update_content.sql.bz2
     rm ./sql_backups/update_content.sql
     echo "SQL Content and Structure Successfully Updated";
@@ -10,7 +10,7 @@ elif [ $1 == structure ]
 then
     cp ./sql_backups/update_structure.sql.bz2 ./sql_backups/update_structure2.sql.bz2
     bunzip2 ./sql_backups/update_structure.sql.bz2
-    /Applications/MAMP/Library/bin/mysql --user=root --password=bjv0623 Codelaborate < ./sql_backups/update_structure.sql
+    /Applications/MAMP/Library/bin/mysql --user=root --password=bjv0623 code < ./sql_backups/update_structure.sql
     mv ./sql_backups/update_structure2.sql.bz2 ./sql_backups/update_structure.sql.bz2
     rm ./sql_backups/update_structure.sql
     echo "SQL Structure Successfully Updated";
--- shell/server/sql_update.sh
+++ shell/server/sql_update.sh
@@ -2,7 +2,7 @@ if [ $1 == content ]
 then
     cp ./sql_backups/update_content.sql.bz2 ./sql_backups/update_content2.sql.bz2
     bunzip2 ./sql_backups/update_content.sql.bz2
-    mysql -p Codelaborate < ./sql_backups/update_content.sql
+    mysql -p code < ./sql_backups/update_content.sql
     mv ./sql_backups/update_content2.sql.bz2 ./sql_backups/update_content.sql.bz2
     rm ./sql_backups/update_content.sql
     echo "SQL Content and Structure Successfully Updated";
@@ -10,7 +10,7 @@ elif [ $1 == structure ]
 then
     cp ./sql_backups/update_structure.sql.bz2 ./sql_backups/update_structure2.sql.bz2
     bunzip2 ./sql_backups/update_structure.sql.bz2
-    mysql -p Codelaborate < ./sql_backups/update_structure.sql
+    mysql -p code < ./sql_backups/update_structure.sql
     mv ./sql_backups/update_structure2.sql.bz2 ./sql_backups/update_structure.sql.bz2
     rm ./sql_backups/update_structure.sql
     echo "SQL Structure Successfully Updated";
