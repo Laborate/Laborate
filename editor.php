7c94687a99093fc33e1e45ada559d7d99847a029
root <root@laborate-dev.(none)>
2013-03-05 09:18:23 +0000

    daily update

--- editor.php
+++ editor.php
@@ -15,7 +15,7 @@ getDependencies(["core", "icons", "editor", "chatroom", "header", "backdrop"]);
     <?php } else { ?>
         <title>Editor<?php echo $_SESSION['webSiteTitle']; ?></title>
     <?php } ?>
-    <script src="http://test:8000/socket.io/socket.io.js"></script>
+    <script src="http://<?php echo $_SERVER['SERVER_ADDR']; ?>:8000/socket.io/socket.io.js"></script>
   	<script type="text/javascript">
   	    try {
   	        window.nodeSocket = io.connect('http://<?php echo $_SERVER['SERVER_ADDR']; ?>:8000');
