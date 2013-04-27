e2b11b8202d2f0e8d4c22bd2e56c69da6b5be862
root <root@laborate-dev.(none)>
2013-04-27 21:00:19 +0000

    update

--- shell/node_control.sh
+++ shell/node_control.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 BASE="$(cd "$(dirname "$0")"; pwd)/../"
 
-if [ -z "$1" ]
-then
+if [ -z "$1" ]; then
     echo "Arguments: start, stop & restart"
 else
-    if [ $1 == start ]
-    then
+    if [ "$1" == "start" ]; then
         node $BASE/server/node/server.js > "$BASE/server/node/logs/`(date +"%T_%m_%d_%Y")`.log" &
         echo "Node Server Started"
-    elif [ $1 == stop ]
-    then
+    elif [ "$1" == "stop" ]; then
         killall node &
         echo "Node Server Stopped"
-    elif [ $1 == restart ]
-    then
+    elif [ "$1" == "restart" ]; then
         killall node &
         sleep 1
         node $BASE/server/node/server.js > "$BASE/server/node/logs/`(date +"%T_%m_%d_%Y")`.log" &
