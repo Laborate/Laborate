66f9d7c06357c5a15ba77665b32871164723963b
root <root@laborate-dev.(none)>
2013-05-04 10:01:51 +0000

    update

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Install After [base-init](https://github.com/Laborate/base-init)
 ------------------------------------------
 **Run Installer Script**
 ```bash
-cd ~; git clone -b node_migration git@github.com:Laborate/code.git; bash code/init/init.sh;
+cd ~; git clone -b node_migration git@github.com:Laborate/code.git; cd code; npm install;
 ```
 
 **Ask Administrator To Reload Apache**
--- start.js
+++ start.js
@@ -1,11 +1,14 @@
 var forever = require('forever-monitor');
 
 var child = new (forever.Monitor)('server.js', {
-max: 3,
-silent: false,
-watch: true,
-watchDirectory: '.',
-//env: { 'NODE_ENV': 'production' }
+    max: 3,
+    silent: false,
+    watch: true,
+    watchDirectory: '.',
+    //env: { 'NODE_ENV': 'production' }
+    //logFile: 'logs/forever.log',
+    //outFile: 'logs/output.log',
+    //errFile: 'logs/errors.log'
 });
 
 child.start();
