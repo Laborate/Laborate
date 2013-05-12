e2250a6e49108de88e747712929f854e508a0a60
Brian Vallelunga <bvallelunga@parkme.com>
2013-05-12 22:26:13 +0000

    update

--- shell/initialize.sh
+++ shell/initialize.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 BASE=~/code
 
+clear;
+
+git submodule init
+git submodule update
+
+$BASE/shell/update_cron.sh
+$BASE/shell/sql_update.sh content
+$BASE/shell/node_control.sh start
 git update-index --assume-unchanged $BASE/config.json
