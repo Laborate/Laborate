9ae14f062434635aae73162f8c4855893b8451ae
Brian Vallelunga <brian@laborate.io>
2013-07-14 05:03:13 +0000

    update

--- routes/account.js
+++ routes/account.js
@@ -1,16 +1,10 @@
-/* Modules: NPM */
-var async = require("async");
-
-/* Modules: Custom */
-var github_lib = require("../lib/github");
-
 exports.index = function(req, res) {
     res.render('account', {
         title: 'Account',
         navigation: 'User Settings',
         mode: "account",
         user: req.session.user,
-        github_auth_url: github_lib.auth_url,
+        github_auth_url: req.github.auth_url,
         js: clientJS.renderTags("account", "header"),
         css: clientCSS.renderTags("account", "header"),
     });
