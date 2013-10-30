0cd9e9b5a5e482170a9e17f6327670ff65913320
Brian Vallelunga <brian@laborate.io>
2013-10-30 02:34:33 +0000

    update

--- start.js
+++ start.js
@@ -36,4 +36,42 @@ var child = new (forever.Monitor)(__dirname + '/server.js', {
 });
 
 //Start Forver
-child.start()
+child.start();
+
+//Update config tmp file
+var config_tmp = {};
+var keep_fields = [
+    "forever", "general", "sentry",
+    "cookies", "cookie_session:key",
+    "orm:dialect", "orm:port", "orm:host",
+    "email", "forever", "github:scope",
+    "development:debugger"
+];
+
+$.each(config, function(key, value) {
+    if(keep_fields.indexOf(key) == -1) {
+        if(typeof value == "object") {
+            config_tmp[key] = {}
+
+            $.each(value, function(inner_key, inner_value) {
+                if(keep_fields.indexOf(key + ":" + inner_key) == -1) {
+                    if(Array.isArray(inner_value)) {
+                        config_tmp[key][inner_key] = [];
+                    } else if(typeof inner_value == "object") {
+                        config_tmp[key][inner_key] = {};
+                    } else {
+                        config_tmp[key][inner_key] = "";
+                    }
+                } else {
+                    config_tmp[key][inner_key] = inner_value;
+                }
+            });
+        } else {
+            config_tmp[key] = "";
+        }
+    } else {
+        config_tmp[key] = value;
+    }
+});
+
+fs.writeFile(__dirname + "/config.tmp.json", JSON.stringify(config_tmp, null, 4));
