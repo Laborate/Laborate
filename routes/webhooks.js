b5583ea497d12b1a79c7e12fef47fbc82dd29c1b
Brian Vallelunga <brian@laborate.io>
2013-11-15 06:27:40 +0000

    update

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -1,56 +1,36 @@
 exports.stripe = function(req, res) {
     switch(req.body.type) {
-        /* Trial Ending Soon */
-        case "customer.subscription.trial_will_end":
-            req.models.users.find({
-                stripe: req.body.data.object.customer
-            }, function(error, users) {
-                if(!error && users.length == 1) {
-                    if(users[0].trial) {
-                        req.models.notifications.create({
-                            message: "Your trial is ending soon, please enter a valid credit card",
-                            priority: true,
-                            user_id: users[0].id
-                        }, blank_function);
-                    }
-
-                    res.send(200);
-                } else {
-                    res.error(200, false, error);
-                }
-            });
-            break;
-
         /* Payment Succeeded */
         case "invoice.payment_succeeded":
-            req.models.users.find({
-                stripe: req.body.data.object.customer
-            }, function(error, users) {
-                if(!error && users.length == 1) {
-                    var date = new Date(req.body.data.object.lines.data[0].period.start * 1000);
-                    var monthNames = [ "January", "Feburary", "March", "April", "May", "June",
-                                       "July", "August", "September", "October", "November", "December" ];
-                    var month = (date.getUTCMonth() == 12) ? 0 : date.getUTCMonth();
+            if(req.body.data.object.lines.data[0].amount != 0) {
+                req.models.users.find({
+                    stripe: req.body.data.object.customer
+                }, function(error, users) {
+                    if(!error && users.length == 1) {
+                        var date = new Date(req.body.data.object.lines.data[0].period.start * 1000);
+                        var monthNames = [ "January", "Feburary", "March", "April", "May", "June",
+                                           "July", "August", "September", "October", "November", "December" ];
+                        var month = (date.getUTCMonth() == 12) ? 0 : date.getUTCMonth();
 
-                    users[0].save({
-                        trial: false,
-                        deliquent: false
-                    }, blank_function);
+                        users[0].save({
+                            deliquent: false
+                        }, blank_function);
 
-                    req.models.payments.create({
-                        description: "Invoice for " + monthNames[month] + " " + date.getFullYear(),
-                        amount: req.body.data.object.lines.data[0].amount/100,
-                        currency: req.body.data.object.lines.data[0].currency,
-                        plan: req.body.data.object.lines.data[0].plan.name,
-                        user_id: users[0].id
-                    }, blank_function);
+                        req.models.payments.create({
+                            description: "Invoice for " + monthNames[month] + " " + date.getFullYear(),
+                            amount: req.body.data.object.lines.data[0].amount/100,
+                            currency: req.body.data.object.lines.data[0].currency,
+                            plan: req.body.data.object.lines.data[0].plan.name,
+                            user_id: users[0].id
+                        }, blank_function);
 
 
-                    res.send(200);
-                } else {
-                    res.error(200, false, error);
-                }
-            });
+                        res.send(200);
+                    } else {
+                        res.error(200, false, error);
+                    }
+                });
+            }
             break;
 
         /* Payment Failed */
