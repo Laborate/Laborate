89e86573330982e69b30b44208f727efb5b5015e
Brian Vallelunga <b.vallelunga@yahoo.com>
2013-12-25 13:37:35 +0000

    update

--- cron/editor/users.js
+++
@@ -1,4 +0,0 @@
-require('../init')("editor.users", function() {
-    var _this = this;
-    _this.finish();
-});
--- cron/index.js
+++ cron/index.js
@@ -24,6 +24,8 @@ module.exports = function(root_dir) {
             tab.remove(tab.findComment("laborate_middleware_restart"));
             tab.create(restartCommand, "laborate_middleware_restart").hour().every(3);
 
+            /* CPU LEAK in the files below
+
             //Users Feedback (On: 1st hour)
             tab.remove(tab.findComment("users_feedback"));
             tab.create(exportCommand + " && node " + path.join(root_dir, "/cron/users/feedback.js"), "users_feedback").hour().on(1);
@@ -36,13 +38,11 @@ module.exports = function(root_dir) {
             tab.remove(tab.findComment("users_deliquent"));
             tab.create(exportCommand + " && node " + path.join(root_dir, "/cron/users/deliquent.js"), "users_deliquent").dom().on(1);
 
-            //Editor Users (Every: 1 Minute)
-            tab.remove(tab.findComment("editor_users"));
-            tab.create(exportCommand + " && node " + path.join(root_dir, "/cron/editor/users.js"), "editor_users");
-
             //Editor Changes (Every: 5 Minutes)
             tab.remove(tab.findComment("editor_changes"));
             tab.create(exportCommand + " && node " + path.join(root_dir, "/cron/editor/changes.js"), "editor_changes").minute().every(5);
+
+            */
 
             //Save Crontab
             tab.save();
