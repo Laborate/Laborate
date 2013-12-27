4336940f484e798f13dcab9c31863066f57b8904
Brian Vallelunga <b.vallelunga@yahoo.com>
2013-12-27 00:57:07 +0000

    update

--- shell/release.sh
+++ shell/release.sh
@@ -1,9 +1,24 @@
 cd ~/middleware;
 
-BRANCH = "$(git symbolic-ref --short HEAD)";
+branch="$(git symbolic-ref HEAD 2>/dev/null)" ||
+branch="(unnamed branch)"
+branch=${branch_name##refs/heads/}
+
+while [[ -z "$merge" || -z "$tag" ]]; do
+    clear;
+    read -p "Branch: " merge;
+    read -p "Tag: " tag;
+    read -p "Is this information correct [Y/n]: " correct;
+
+    if [ "$correct" == "n" ]; then
+        merge=;
+        tag=;
+    fi
+done
+
 git checkout master;
 git pull --rebase;
-git merge $1;
-git tag $2;
+git merge $merge;
+git tag $tag;
 git push origin master;
-git checkout $BRANCH;
+git checkout $branch;
