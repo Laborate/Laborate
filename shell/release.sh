c22d5d2149cc0c47e2ce4e730c247e51f7780800
Brian Vallelunga <b.vallelunga@yahoo.com>
2013-12-27 00:37:40 +0000

    update

--- shell/release.sh
+++ shell/release.sh
@@ -1,7 +1,9 @@
 cd ~/middleware;
 
+BRANCH= $(git branch | sed -n -e 's/^\* \(.*\)/\1/p');
 git checkout master;
 git pull --rebase;
 git merge $1;
 git tag -s $2 -m 'Branch $1 signed by $USER';
 git push origin master;
+git checkout $BRANCH;
