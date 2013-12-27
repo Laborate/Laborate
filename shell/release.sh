c0b8e07a9c37b23828dc7aea8cab221c8c3e3435
Brian Vallelunga <b.vallelunga@yahoo.com>
2013-12-27 00:47:42 +0000

    update

--- shell/release.sh
+++ shell/release.sh
@@ -4,6 +4,6 @@ BRANCH = "$(git symbolic-ref --short HEAD)";
 git checkout master;
 git pull --rebase;
 git merge $1;
-git tag -s $2 -m 'Branch $1 signed by $USER';
+git tag $2;
 git push origin master;
 git checkout $BRANCH;
