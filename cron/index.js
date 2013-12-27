3635084d8930db85747f6d4904bb5cb76b7689b7
Brian Vallelunga <b.vallelunga@yahoo.com>
2013-12-27 06:10:50 +0000

    update

--- cron/index.js
+++ cron/index.js
@@ -10,6 +10,7 @@ module.exports = function(root_dir) {
             var npmBinRoot = path.join(npmPrefix, 'bin');
             var nodePath = process.execPath.split('/').slice(0, -1).join('/');
             var exportCommand = 'export PATH=' + nodePath + ':$PATH';
+            var nodeCommand = exportCommand + " && node ";
 
             //Start (On Reboot)
             var foreverCommand = path.join(npmBinRoot, 'forever');
@@ -21,28 +22,25 @@ module.exports = function(root_dir) {
 
             //Users Feedback (On: 1st hour)
             tab.remove(tab.findComment("users_feedback"));
-            tab.create(exportCommand + " && node " + path.join(root_dir, "/cron/users/feedback.js"), "users_feedback").hour().on(1);
+            tab.create(nodeCommand + path.join(root_dir, "/cron/users/feedback.js"), "users_feedback").hour().on(1);
 
             //Users Feedback Notifications (On: 1st hour)
             tab.remove(tab.findComment("users_feedback_notifications"));
             if(config.feedback.enabled) {
-                tab.create(
-                    exportCommand + " && node " + path.join(root_dir, "/cron/users/feedback_notifications.js"),
-                    "users_feedback_notifications"
-                ).hour().on(1);
+                tab.create(nodeCommand + path.join(root_dir, "/cron/users/feedback_notifications.js"), "users_feedback_notifications").hour().on(1);
             }
 
             //Users Tracking (Every: 10 minutes)
             tab.remove(tab.findComment("users_tracking"));
-            tab.create(exportCommand + " && node " + path.join(root_dir, "/cron/users/tracking.js"), "users_tracking").minute().every(10);
+            tab.create(nodeCommand + path.join(root_dir, "/cron/users/tracking.js"), "users_tracking").minute().every(10);
 
             //Users Deliquent (Every: 1 Month)
             tab.remove(tab.findComment("users_deliquent"));
-            tab.create(exportCommand + " && node " + path.join(root_dir, "/cron/users/deliquent.js"), "users_deliquent").dom().on(1);
+            tab.create(nodeCommand + path.join(root_dir, "/cron/users/deliquent.js"), "users_deliquent").dom().on(1);
 
             //Editor Changes (Every: 5 Minutes)
             tab.remove(tab.findComment("editor_changes"));
-            tab.create(exportCommand + " && node " + path.join(root_dir, "/cron/editor/changes.js"), "editor_changes").minute().every(5);
+            tab.create(nodeCommand + path.join(root_dir, "/cron/editor/changes.js"), "editor_changes").minute().every(5);
 
             //Save Crontab
             tab.save();
--- cron/users/feedback_notification.js
+++ cron/users/feedback_notification.js
@@ -4,13 +4,14 @@ require('../init')("user.feedback", function() {
 
     _this.models.users.find({
         feedback: false,
+        feedback_asked: false
     }, function(error, users) {
         if(!error && !users.empty) {
             $.each(users, function(key, user) {
                 var timeDiff = Math.abs(today.getTime() - user.created.getTime());
                 var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
-                if(diffDays >= 2 && user.feedbacks.empty) {
+                if(diffDays >= 3 && user.feedbacks.empty) {
                     _this.models.notifications.create({
                         message: "We noticed you have been using our service, \
                         for a couple days, do you have any <a href='/feedback/'>feedback</a>?",
@@ -28,9 +29,15 @@ require('../init')("user.feedback", function() {
                         }, function(error) {
                             lib.error.capture(error);
 
-                            if(users.end(key)) {
-                                _this.finish();
-                            }
+                            user.save({
+                                feedback_asked: true
+                            }, function(error) {
+                                lib.error.capture(error);
+
+                                if(users.end(key)) {
+                                    _this.finish();
+                                }
+                            });
                         });
                     });
                 } else if(users.end(key)) {
