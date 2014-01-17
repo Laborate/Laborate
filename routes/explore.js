08c3487e0eb47883381a2c307784f9477ef25cb9
Brian Vallelunga <b.vallelunga@yahoo.com>
2014-01-17 06:23:12 +0000

    updat

--- routes/explore.js
+++ routes/explore.js
@@ -2,43 +2,18 @@ var async = require('async');
 
 exports.index = function(req, res, next) {
     var hot_documents = [];
-    req.models.documents.page(1).order("viewed", "Z").run(function (error, documents) {
-        async.each(documents, function(document, callback) {
-            var users = [];
-
-            async.each(document.roles, function(role, callback) {
-                role.getUser(function(error, user) {
-                    users.push(user);
-                    callback(error);
-                });
-            }, function(errors) {
-                document.getOwner(function(error, owner) {
-                    callback(errors);
-
-                    hot_documents.push({
-                        document: {
-                            pub_id: document.pub_id,
-                            name: document.name,
-                            size: document.size(true)
-                        },
-                        owner: owner.screen_name,
-                        users: users
-                    });
-                });
+    req.models.documents.all({}, {
+        autoFetch:true
+    }, 10, ["viewed", "Z"], function (error, documents) {
+        if(!error && !documents.empty) {
+            res.renderOutdated('explore/index', {
+                title: 'Explore',
+                documents: documents,
+                js: clientJS.renderTags("explore", "backdrop"),
+                css: clientCSS.renderTags("explore", "backdrop"),
+                backdrop: req.backdrop(),
+                pageTrack: true
             });
-        }, function(errors) {
-            if(!errors) {
-                res.renderOutdated('explore/index', {
-                    title: 'Explore',
-                    documents: hot_documents,
-                    js: clientJS.renderTags("explore", "backdrop"),
-                    css: clientCSS.renderTags("explore", "backdrop"),
-                    backdrop: req.backdrop(),
-                    pageTrack: true
-                });
-            } else {
-                res.error(404, null, errors);
-            }
-        });
+        }
     });
 }
