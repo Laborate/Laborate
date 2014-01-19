d7d75a7b313e87038994c3fe95908eaf2b0ab3fb
Brian Vallelunga <b.vallelunga@yahoo.com>
2014-01-19 06:21:13 +0000

    update

--- public/js/welcome/index.js
+++ public/js/welcome/index.js
@@ -71,15 +71,20 @@ function socialInit() {
         var data = JSON.parse($(this).attr("data-documents"));
 
         if(data.length != 0) {
-            var canvas = $(this).get()[0];
-            var graph = canvas.getContext("2d");
+            var max = Math.max.apply(null, data);
+            var min = Math.min.apply(null, data);
+            var scaler = (max - min)/100;
+
             var width = 5;
             var rightShift = 30;
 
+            var canvas = $(this).get()[0];
+            var graph = canvas.getContext("2d");
             graph.fillStyle="rgba(255, 255, 255, .2)";
 
             $.each(data, function(key, height) {
-                graph.fillRect(rightShift, canvas.height-height, width, height);
+                height = height / scaler;
+                graph.fillRect(rightShift, canvas.height - height, width, height);
                 rightShift += width + 1;
             });
         } else {
