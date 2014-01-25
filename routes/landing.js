d8111739dc7e84a1fd235374dcd88577a8787761
Brian Vallelunga <b.vallelunga@yahoo.com>
2014-01-25 11:55:08 +0000

    update

--- routes/landing.js
+++ routes/landing.js
@@ -1,12 +1,17 @@
 exports.index = function(req, res) {
-    res.renderOutdated('landing/index', {
-        title: "Collaborate Anywhere",
-        user: req.session.user,
-        js: clientJS.renderTags("landing", "codemirror", "codemirror-movie"),
-        css: clientCSS.renderTags("backdrop", "landing", "codemirror", "codemirror-movie"),
-        backdrop: req.backdrop(),
-        pageTrack: false,
-        title_first: false
+    req.models.users.all({
+        id: req.db.tools.ne((req.session.user) ? req.session.user.id : null)
+    }).limit(15).orderRaw("rand()").run(function(error, users) {
+        res.renderOutdated('landing/index', {
+            title: "Collaborate Anywhere",
+            user: req.session.user,
+            laborators: users,
+            js: clientJS.renderTags("landing", "codemirror", "codemirror-movie"),
+            css: clientCSS.renderTags("backdrop", "landing", "codemirror", "codemirror-movie"),
+            backdrop: req.backdrop(),
+            pageTrack: false,
+            title_first: false
+        });
     });
 
     if(!req.session.landing) {
