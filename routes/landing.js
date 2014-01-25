479be1e76b022fb8458fc55f991a34d6efc5ef87
Brian Vallelunga <b.vallelunga@yahoo.com>
2014-01-25 12:28:09 +0000

    update

--- routes/landing.js
+++ routes/landing.js
@@ -1,21 +1,33 @@
-exports.index = function(req, res) {
-    req.models.users.all({
-        id: req.db.tools.ne((req.session.user) ? req.session.user.id : null)
-    }).limit(15).orderRaw("rand()").run(function(error, users) {
-        res.renderOutdated('landing/index', {
-            title: "Collaborate Anywhere",
-            user: req.session.user,
-            laborators: users,
-            js: clientJS.renderTags("landing", "codemirror", "codemirror-movie"),
-            css: clientCSS.renderTags("backdrop", "landing", "codemirror", "codemirror-movie"),
-            backdrop: req.backdrop(),
-            pageTrack: false,
-            title_first: false
-        });
-    });
+var async = require('async');
 
-    if(!req.session.landing) {
-        req.session.landing = true;
-        req.session.save();
-    }
+exports.index = function(req, res) {
+    async.parallel({
+        laborators: function(callback) {
+            req.models.users.find({
+                id: req.db.tools.ne((req.session.user) ? req.session.user.id : null),
+                admin: false
+            }).limit(15).orderRaw("rand()").run(callback);
+        },
+        admins: function(callback) {
+            req.models.users.find({
+                admin: true
+            }).limit(3).run(callback);
+        }
+    }, function(errors, data) {
+        if(!errors) {
+            res.renderOutdated('landing/index', {
+                title: "Collaborate Anywhere",
+                user: req.session.user,
+                laborators: data.laborators,
+                admins: data.admins,
+                js: clientJS.renderTags("landing", "codemirror", "codemirror-movie"),
+                css: clientCSS.renderTags("backdrop", "landing", "codemirror", "codemirror-movie"),
+                backdrop: req.backdrop(),
+                pageTrack: false,
+                title_first: false
+            });
+        } else {
+            res.error(500, "Laborate is currently experiencing technical difficulties.", errors);
+        }
+    });
 };
