f25eaa869c8250719eba040afca4d91fdab8b190
Brian Vallelunga <b.vallelunga@yahoo.com>
2014-02-09 09:25:29 +0000

    updat

--- routes/welcome.js
+++ routes/welcome.js
@@ -45,46 +45,73 @@ exports.gravatar = function(req, res) {
 };
 
 exports.social = function(req, res) {
-    req.models.users.all({
-        id: req.db.tools.ne(req.session.user.id)
-    }).limit(10).orderRaw("rand()").run(function(error, users) {
-        if(!error) {
-            async.mapSeries(users, function(user, callback) {
-                req.models.documents.roles.find({
-                    user_id: user.id
-                }, ["viewed"], 60, ["created", "Z"], function(error, roles) {
-                    user.documents = $.map(roles, function(role) {
-                        return role.viewed;
-                    });
-                    callback(null, user);
-                });
-            }, function(error, laborators) {
-                if(!error) {
-                    res.renderOutdated('welcome/social', {
-                        title: 'Welcome',
-                        users: laborators.sort(function(a, b) {
-                            if(a.documents.length === b.documents.length) {
-                                var c = (a.documents.empty) ? 0 : Math.max.apply(null, a.documents);
-                                var d = (b.documents.empty) ? 0 : Math.max.apply(null, b.documents);
-                            } else {
-                                var c = a.documents.length;
-                                var d = b.documents.length;
-                            }
+    var users = [];
+    var screen_names = [];
+    var counter = 0;
 
-                            return (c > d) ? -1 : ((c < d) ? 1 : 0);
-                        }),
-                        js: clientJS.renderTags("backdrop", "welcome"),
-                        css: clientCSS.renderTags("backdrop", "welcome"),
-                        backdrop: req.backdrop(),
-                        pageTrack: true
+    req.models.users.count(function(error, count) {
+        async.until(
+            function() {
+                return (counter > 9) || (counter > count);
+            },
+            function(next) {
+                req.models.users.find({
+                    id: req.db.tools.ne((req.session.user) ? req.session.user.id : null),
+                    admin: false
+                })
+                .only(["name", "screen_name", "email"])
+                .skip(Math.floor(Math.random() * count))
+                .limit(1)
+                .run(function(error, user) {
+                    if(!user.empty && screen_names.indexOf(user[0].screen_name) == -1) {
+                        counter++;
+                        users.push(user[0]);
+                        screen_names.push(user[0].screen_name);
+                    }
+
+                    next(error);
+                });
+            },
+            function(errors) {
+                if(!errors) {
+                    async.mapSeries(users, function(user, callback) {
+                        req.models.documents.roles.find({
+                            user_id: user.id
+                        }, ["viewed"], 60, ["created", "Z"], function(error, roles) {
+                            user.documents = $.map(roles, function(role) {
+                                return role.viewed;
+                            });
+                            callback(null, user);
+                        });
+                    }, function(error, laborators) {
+                        if(!error) {
+                            res.renderOutdated('welcome/social', {
+                                title: 'Welcome',
+                                users: laborators.sort(function(a, b) {
+                                    if(a.documents.length === b.documents.length) {
+                                        var c = (a.documents.empty) ? 0 : Math.max.apply(null, a.documents);
+                                        var d = (b.documents.empty) ? 0 : Math.max.apply(null, b.documents);
+                                    } else {
+                                        var c = a.documents.length;
+                                        var d = b.documents.length;
+                                    }
+
+                                    return (c > d) ? -1 : ((c < d) ? 1 : 0);
+                                }),
+                                js: clientJS.renderTags("backdrop", "welcome"),
+                                css: clientCSS.renderTags("backdrop", "welcome"),
+                                backdrop: req.backdrop(),
+                                pageTrack: true
+                            });
+                        } else {
+                            res.error(404, null, error);
+                        }
                     });
                 } else {
-                    res.error(404, null, error);
+                    res.error(404, null, errors);
                 }
-            });
-        } else {
-            res.error(404, null, error);
-        }
+            }
+        )
     });
 };
 
