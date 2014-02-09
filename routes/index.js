7c7931349721d72acdbfc5bbef406efba84a1e9c
Brian Vallelunga <b.vallelunga@yahoo.com>
2014-02-09 11:07:46 +0000

    update

--- routes/index.js
+++ routes/index.js
@@ -98,7 +98,7 @@ module.exports = function(app) {
     app.post('/documents/file/create', authUtil.restrictAccess, authUtil.xhr, documents.file_create);
     app.post('/documents/file/upload', authUtil.restrictAccess, authUtil.xhr, documents.file_upload);
     app.post('/documents/file/:document/rename', authUtil.restrictAccess, authUtil.xhr, documents.file_rename);
-    app.post('/documents/file/:document/private', authUtil.restrictAccess, authUtil.xhr, documents.file_private);
+    app.post('/documents/file/:document/private', authUtil.restrictAccess, authUtil.xhr, documents.stats, documents.file_private);
     app.post('/documents/file/:document/remove', authUtil.restrictAccess, authUtil.xhr, documents.file_remove);
     app.post('/documents/location/create', authUtil.restrictAccess, authUtil.xhr, documents.create_location);
 
