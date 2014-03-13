cbd216c51c4b65bc7af6510e4f2638d43bf36d18
Brian Vallelunga <vallelungabrian@gmail.com>
2014-03-13 00:50:01 +0000

    update

--- public/js/groups/util.js
+++ public/js/groups/util.js
@@ -6,26 +6,24 @@ window.groupsUtil = {
                         .val("loading...")
                         .addClass("disabled");
 
-        if(form.find(".input").val()) {
-            $.post(form.attr("action"), {
-                _csrf: window.config.csrf,
-                name: form.find(".input").val(),
-                description: form.find(".textarea").val(),
-            }, function(data) {
-                if(data.success) {
-                    window.location.href = "/groups/" + data.group + "/";
-                } else {
-                    submit
-                        .val(data.error_message)
-                        .addClass("error")
-                        .removeClass("disabled");
+        $.post(form.attr("action"), {
+            _csrf: window.config.csrf,
+            name: form.find(".input").val(),
+            description: form.find(".textarea").val(),
+        }, function(data) {
+            if(data.success) {
+                window.location.href = "/groups/" + data.group + "/";
+            } else {
+                submit
+                    .val(data.error_message)
+                    .addClass("error")
+                    .removeClass("disabled");
 
-                    clearInterval(_this.timer);
-                    _this.timer = setTimeout(function() {
-                        submit.val(submit.attr("data-original")).removeClass("error");
-                    }, 5000);
-                }
-            });
-        }
+                clearInterval(_this.timer);
+                _this.timer = setTimeout(function() {
+                    submit.val(submit.attr("data-original")).removeClass("error");
+                }, 5000);
+            }
+        });
     }
 }
